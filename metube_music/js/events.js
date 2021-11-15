8abd7cf11429265ba3a9b266714f140a8cb808f3
sangpok <y_y5760y_y@naver.com>
2021-11-15 17:15:40 +0900

    211115

--- metube_music/js/events.js
+++ metube_music/js/events.js
@@ -1,3 +1,5 @@
+// -------------------------------------------------------- Search 관련
+
 const $btnSearch = document.querySelector("#btnSearch");
 const $btnSearchBack = document.querySelector("#btnSearchBack");
 
@@ -18,3 +20,66 @@ const btnSearch_Click = function(e) {
 
 $btnSearch.addEventListener("click", btnSearch_Click);
 $btnSearchBack.addEventListener("click", btnSearch_Click);
+
+// -------------------------------------------------------- nav 관련
+
+const ControlItemWrapper = function() {
+    
+}
+
+const SlideTo = function(e) {
+    let $items = e.srcElement;
+    if ($items.tagName == "I") $items = e.srcElement.parentElement;
+
+    let isNext = false;
+    isNext = $items.classList.contains("btn_nxt");
+    
+    $items = $items.parentElement.firstElementChild.firstElementChild;
+
+    const itemsStyle = window.getComputedStyle($items);
+    let matrix = new WebKitCSSMatrix(itemsStyle.transform);
+    let curPos = matrix.m41;
+
+    let addAmount = $items.parentElement.getBoundingClientRect().width;
+    // let tmpEle = document.elementFromPoint(addAmount, 180);
+    // console.log(tmpEle);
+
+    curPos = isNext ? curPos - addAmount : curPos + addAmount;
+    // if (isNext) {
+    //     if (curPos - addAmount)
+    // } else {
+
+    // }
+
+    $items.style.transform = `translateX(${curPos}px)`;
+    ControlItemWrapper();
+}
+
+const $btnPrvs = document.querySelectorAll("button.btn_prv");
+$btnPrvs.forEach(element => {
+    element.addEventListener("click", SlideTo);
+});
+
+const $btnNxts = document.querySelectorAll("button.btn_nxt");
+$btnNxts.forEach(element => {
+    element.addEventListener("click", SlideTo);
+});
+
+let curIntersectionElements = [];
+
+const itemsWrappers = document.querySelectorAll(".itemsWraper");
+itemsWrappers.forEach(itemsWrapper => {
+    curIntersectionElements.push(itemsWrapper);
+    const io = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                if (entry.intersectionRatio < 1) { // 걸침
+                    entry.target.style.border = "2px solid red";
+                }
+            }
+        });
+    }, {"root": itemsWrapper});
+
+    const $itemsAll = itemsWrapper.querySelectorAll(".item");
+    $itemsAll.forEach(e => io.observe(e));
+})
